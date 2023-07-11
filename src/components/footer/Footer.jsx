7b109ee53bffb82c137498aa9a2ea51e66313bf1
Akhil Bolla <akhilbolla89@gmail.com>
2023-07-11 17:57:16 +0000

    Your commit message

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,38 +1,51 @@
-import React from 'react';
+import React from "react";
 import "./footer.css";
-import { FiTwitter, FiGithub, FiLinkedin } from 'react-icons/fi';
+import { FiGithub, FiLinkedin, FiInstagram } from "react-icons/fi";
+import { AiOutlineWhatsApp } from "react-icons/ai";
 
 const Footer = () => {
   return (
     <footer className="footer">
-        <div className="footer__container container">
-            <h1 className="footer__title">Cyphr</h1>
-            <ul className="footer__list">
-                <li>
-                    <a href="#about" className="footer__link">About</a>
-                </li>
-                <li>
-                    <a href="#portfolio" className="footer__link">Projects</a>
-                </li>
-                <li>
-                    <a href="#testimonials" className="footer__link">Testimonials</a>
-                </li>
-            </ul>
-            <div className="footer__social">
-                <a href="https://twitter.com/cyphrsylph" className="home__social-icon" target="_blank" rel="noreferrer">
-                    <FiTwitter />
-                </a>
-                <a href="https://www.github.com/kmaalig" className="home__social-icon" target="_blank" rel="noreferrer">
-                    <FiGithub />
-                </a>
-                <a href="https://www.linkedin.com/in/kmaalig/" className="home__social-icon" target="_blank" rel="noreferrer" >
-                    <FiLinkedin />
-                </a>    
-            </div>
-            <span className="footer__copy"></span>
+      <div className="footer__container container">
+        <h1 className="footer__title"> - DEV AKHIL - </h1>
+        <div className="footer__social">
+          <a
+            href="https://wa.me/qr/WAQNAZ4SKIPJJ1"
+            className="home__social-icon"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <AiOutlineWhatsApp />
+          </a>
+          <a
+            href="https://github.com/akhilbolla18"
+            className="home__social-icon"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FiGithub />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/akhilbolla"
+            className="home__social-icon"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FiLinkedin />
+          </a>
+          <a
+            href="https://instagram.com/hey_akhil1?igshid=YmMyMTA2M2Y="
+            className="home__social-icon"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FiInstagram />
+          </a>
         </div>
+        <span className="footer__copy"></span>
+      </div>
     </footer>
   );
-}
+};
 
 export default Footer;
--- src/components/home/Social.jsx
+++ src/components/home/Social.jsx
@@ -1,20 +1,32 @@
 import React from "react";
-import { FiTwitter, FiGithub, FiLinkedin} from "react-icons/fi";
+import { FiTwitter, FiGithub, FiLinkedin } from "react-icons/fi";
 
 const Social = () => {
-    return (
-        <div className="home__social">
-            <a href="https://twitter.com/cyphrsylph" className="home__social-icon" target="_blank">
-                <FiTwitter />
-            </a>
-            <a href="https://www.github.com/kmaalig" className="home__social-icon" target="_blank">
-                <FiGithub />
-            </a>
-            <a href="https://www.linkedin.com/in/kmaalig/" className="home__social-icon" target="_blank">
-                <FiLinkedin />
-            </a>
-        </div> 
-    ); 
-}
+  return (
+    <div className="home__social">
+      {/* <a
+        href="https://twitter.com/cyphrsylph"
+        className="home__social-icon"
+        target="_blank"
+      >
+        <FiTwitter />
+      </a> */}
+      <a
+        href="https://www.linkedin.com/in/akhilbolla"
+        className="home__social-icon"
+        target="_blank"
+      >
+        <FiGithub />
+      </a>
+      <a
+        href="https://github.com/akhilbolla18"
+        className="home__social-icon"
+        target="_blank"
+      >
+        <FiLinkedin />
+      </a>
+    </div>
+  );
+};
 
 export default Social;
